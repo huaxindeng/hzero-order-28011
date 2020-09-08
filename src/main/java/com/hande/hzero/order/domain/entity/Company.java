6f096080df93d0cdfdd9de37d0bfe1090db3271d
huaxin <2502353032@qq.com>
2020-09-08 16:05:02 +0800

    [TEST] 修改4

--- src/main/java/com/hande/hzero/order/domain/entity/Company.java
+++ src/main/java/com/hande/hzero/order/domain/entity/Company.java
@@ -22,6 +22,7 @@ import javax.persistence.*;
 public class Company extends AuditDomain {
 
     public static final String FIELD_COMPANY_NAME = "companyName";
+
     public static final String FIELD_ENABLED_FLAG = "enabledFlag";
 
     @ApiModelProperty("表ID，主键，供其他表做外键")
