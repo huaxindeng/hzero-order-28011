d5ae8fbf96ff3bbf86e07ca823c3a1f410f08d19
huaxindeng <31794302+huaxindeng@users.noreply.github.com>
2020-09-08 16:21:17 +0800

    Revert "Bugfix ag 8003"

--- src/main/java/com/hande/hzero/order/domain/entity/Company.java
+++ src/main/java/com/hande/hzero/order/domain/entity/Company.java
@@ -22,8 +22,6 @@ import javax.persistence.*;
 public class Company extends AuditDomain {
 
     public static final String FIELD_COMPANY_NAME = "companyName";
-
-
     public static final String FIELD_ENABLED_FLAG = "enabledFlag";
 
     @ApiModelProperty("表ID，主键，供其他表做外键")
